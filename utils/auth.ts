f5f3b7cddccd947395db4fdd68e6da6d9a1d0d88
SannidhanKatta <sannidhankatta@gmail.com>
2025-02-19 22:52:13 +0530

    ...

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,11 +1,21 @@
 import { auth } from "@clerk/nextjs";
 import { prisma } from "./db";
+import { redirect } from "next/navigation";
 
 export const getUserByClerkId = async () => {
   const { userId } = auth();
-  const user = await prisma.user.findUniqueOrThrow({
-    where: { clerkId: userId ?? "" },
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: { clerkId: userId },
   });
+
+  if (!user) {
+    redirect("/new-user");
+  }
 
   return user;
 };
